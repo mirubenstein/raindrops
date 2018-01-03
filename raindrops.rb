4715da872898555edf61d78a46f0d225a1cbe326
Mark R <mirubenstein@gmail.com>
2018-01-02 22:20:51 -0800

    update based on review by kotp

--- raindrops.rb
+++ raindrops.rb
@@ -1,10 +1,18 @@
+module NumericExtension
+  refine Numeric do
+    def factor_of?(other)
+      (other % self).zero?
+    end
+  end
+end
+
+
 class RainSounds
+  using NumericExtension
   class << self
-    def for(number) # TODO still order dependant. What should know what to concat? Another class needed?
+    def for(number)
       ObjectSpace.each_object(Class).select { |klass| klass < self }.each_with_object([]) do |rain_sound, collection|
-        collection << rain_sound.new(number) if rain_sound.factor_of number
-      end.each_with_object('') do |rain_sound_class, string|
-        string.concat(rain_sound_class.to_s)
+        collection << rain_sound.new(number) if rain_sound.to_s[-1].to_i.factor_of? number
       end
     end
   end
@@ -16,50 +24,30 @@ class RainSounds
 end
 
 class RainSound7 < RainSounds
-  def self.factor_of(number)
-    (number % 7).zero?
-  end
-
   def to_s
     'Plong'
   end
 end
 
 class RainSound5 < RainSounds
-  def self.factor_of(number)
-    (number % 5).zero?
-  end
-
   def to_s
     'Plang'
   end
 end
 
 class RainSound3 < RainSounds
-  def self.factor_of(number)
-    (number % 3).zero?
-  end
-
   def to_s
     'Pling'
-  end
-end
-
-class RainSoundBase < RainSounds
-  def self.factor_of(number)
-    @number = number
-    !(number % 3).zero? && !(number % 5).zero? && !(number % 7).zero?
-  end
-
-  def to_s
-    @number.to_s
   end
 end
 
 
 class Raindrops
   def self.convert(number)
-    RainSounds.for number
+    rain_sounds = RainSounds.for(number).each_with_object('') do |rain_sound_class, string|
+      string.concat(rain_sound_class.to_s)
+    end
+    rain_sounds.empty? ? number.to_s : rain_sounds
   end
 end
 
